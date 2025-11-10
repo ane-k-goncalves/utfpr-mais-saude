0d5ffd9b26b6400e297c2e801f8dc21108a4cc66
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-11-10 18:16:32 -0300

    create login component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from "./navbar/navbar";
-import { Footer } from './footer/footer';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+import { LoginComponent } from './pages/login/login.component';
+import { Logo } from './components/logo/logo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar,Footer],
+  imports: [RouterOutlet, Navbar, Footer, LoginComponent, Logo],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('utfpr-mais-saude');
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { FormLogin } from '../../components/form-login/form-login';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormLogin],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
