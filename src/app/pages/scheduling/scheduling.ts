6d30b5029a305ad3d3eb59928f8327f56a18c3f9
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-11-16 15:36:25 -0300

    created scheduling screen

---
+++ src/app/pages/scheduling/scheduling.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scheduling',
+  imports: [],
+  templateUrl: './scheduling.html',
+  styleUrl: './scheduling.css'
+})
+export class Scheduling {
+
+}
