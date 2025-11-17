4eb60b5b0bca6ca4a18c2fe36c0445e2fd6eb26e
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-11-17 14:06:39 -0300

    changed form-login to authenticate for pocktbase

--- src/app/components/form-login/form-login.ts
+++ src/app/components/form-login/form-login.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../services/auth';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './form-login.html',
   styleUrl: './form-login.css',
 })
 export class FormLogin {
-  constructor(private router: Router) {}
+  email: string = '';
+  password: string = '';
 
-  onSubmit() {
-    this.router.navigate(['/home']);
+  constructor(private router: Router, private auth: Auth) {}
+
+  async onSubmit() {
+    try {
+      await this.auth.login(this.email, this.password);
+      console.log('usuario logado');
+      this.router.navigate(['/home']);
+    } catch (err) {
+      console.error('falha no login', err);
+      alert('email ou senha invalidos');
+    }
   }
 }
