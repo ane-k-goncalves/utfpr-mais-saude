fdeb93bb830e7c0baa047171cb4e529b228de1ab
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-11-17 10:04:16 -0300

    services/upas file  changed to receive information upas collection come of pocketbase

--- src/app/services/upas.ts
+++ src/app/services/upas.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
+import PocketBase from 'pocketbase';
 import { CardInfoUpa } from '../interfaces/card-info-upa.model';
-import { dataInfoUpa } from '../../mocks/mock-info-upas';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Upas {
+  private pb = new PocketBase('http://127.0.0.1:8090');
+
   constructor() {}
 
-  getUpas(): CardInfoUpa[] {
-    return dataInfoUpa;
+  async getUpas(): Promise<CardInfoUpa[]> {
+    return await this.pb.collection('information_upas').getFullList();
+  }
+
+  async addUpa(upa: CardInfoUpa) {
+    return await this.pb.collection('information_upas').create(upa);
+  }
+
+  async updateUpa(id: string, upa: Partial<CardInfoUpa>) {
+    return await this.pb.collection('information_upas').update(id, upa);
+  }
+
+  async deleteUpa(id: string) {
+    return await this.pb.collection('information_upas').delete(id);
   }
 }
