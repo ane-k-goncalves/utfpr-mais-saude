5da22ed147d6eb618e60daa694a4b85721cf8071
Tiago Sampaio Cordeiro <145937944+tiago-sampaio-cordeiro@users.noreply.github.com>
2025-11-30 17:16:53 -0300

    adicionando parametros via rotas (#54)

--- src/app/pages/historico-consultas/historico-consultas.ts
+++ src/app/pages/historico-consultas/historico-consultas.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-historico-consultas',
   imports: [],
   templateUrl: './historico-consultas.html',
-  styleUrl: './historico-consultas.css'
+  styleUrl: './historico-consultas.css',
 })
 export class HistoricoConsultas {
+  idRecebido!: string | null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.idRecebido = this.route.snapshot.paramMap.get('id');
+    console.log('ID recebido na página de vacinas:', this.idRecebido);
+  }
 }
--- src/app/pages/historico-vacinas/historico-vacinas.ts
+++ src/app/pages/historico-vacinas/historico-vacinas.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-historico-vacinas',
   imports: [],
   templateUrl: './historico-vacinas.html',
-  styleUrl: './historico-vacinas.css'
+  styleUrl: './historico-vacinas.css',
 })
 export class HistoricoVacinas {
+  idRecebido!: string | null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.idRecebido = this.route.snapshot.paramMap.get('id');
+    console.log('ID recebido na página de vacinas:', this.idRecebido);
+  }
 }
