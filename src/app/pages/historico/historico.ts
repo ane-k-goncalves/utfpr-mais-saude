273b98d16aacead90954add32cfc26bbb4466c0a
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-11-30 21:46:00 -0300

    corrigindo import do componente historico

--- src/app/pages/historico/historico.ts
+++ src/app/pages/historico/historico.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CardHistory } from '../../components/card-history/card-history';
 import { Router, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-historico',
-  imports: [CardHistory, RouterOutlet, CommonModule],
+  imports: [CardHistory, RouterOutlet, CommonModule, FormsModule],
   templateUrl: './historico.html',
   styleUrl: './historico.css',
 })
